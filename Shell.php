4643e79810b84165a216fcf7cc526c75f59b9d3e
John P. Bloch <john@avendimedia.com>
2012-07-10 15:13:04 -0400

    Add documentation to Shell.php

--- Shell.php
+++ Shell.php
@@ -1,11 +1,60 @@
 <?php
+
+/**
+ * Shell.php
+ *
+ * This file defines the abstract Shell class that various shell implementations
+ * will extend.
+ *
+ * @author John P. Bloch <john@avendimedia.com>
+ * @version 1.0
+ * @license http://www.gnu.org/copyleft/gpl.html GPLv3 or later
+ */
 namespace Shell;
 
 abstract class Shell
 {
+    /**
+     * This constant will be used as the shell invocation command.
+     *
+     * Since it will be opened with PHP's proc_open() function, this is the
+     * command you would use to open an instance of the shell, not to execute a
+     * command without opening a new process. For example, using the Bourne
+     * shell, to execute a command without opening a new process, you would do
+     *
+     *     sh -c "foo bar"
+     *
+     * However, since we're opening a new process and then executing a command,
+     * it suffices to use simply
+     *
+     *     sh
+     *
+     * @since 1.0
+     */
 
     const SHELL = 'sh';
 
+    /**
+     * Executes a command in the class' specified shell.
+     *
+     * You do not need to run the command through escapeshellcmd(). The
+     * executable file will do this for you before actually running your
+     * command.
+     *
+     * If the optional $args parameter is used, it will be appended to the
+     * command, separated by a space. It will not be escaped. Any values that
+     * need escaping should be run through escapeshellarg() beforehand.
+     *
+     * Errors and output from the command, if any, will be returned in that
+     * order.
+     *
+     * @see escapeshellcmd()
+     * @see escapeshellarg()
+     * @since 1.0
+     * @param string $command The command to execute
+     * @param string $args Optional arguments to append to the command
+     * @return string The error or output information from the command (if any)
+     */
     final public static function exec($command, $args = null)
     {
         $shell = static::SHELL;
@@ -14,6 +63,7 @@ abstract class Shell
             $command = "$command $args";
         }
         $command = trim($command);
+        // Don't bother running if the command wouldn't run anyway.
         if (preg_match('/(^| )noRecursionPlease$/', $command)) {
             return '';
         }
